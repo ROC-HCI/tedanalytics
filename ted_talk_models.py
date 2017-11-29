8dc74a27db73c36019098244718bd9648eebc265
Iftekhar Tanveer <go2chayan@gmail.com>
2017-11-29 16:42:44 -0500

    SyntacticSemanticEngine Implemented

--- ted_talk_models.py
+++ ted_talk_models.py
@@ -9,13 +9,36 @@ import numpy as np
 from torch.nn.parameter import Parameter
 from TED_data_location import ted_data_path, wordvec_path
 
+class multiLinear(nn.Module):
+    '''
+    An embedding module to learn "dic_len" number of affine transformations.
+    Similar to linear but more than one pair of weight and biases.
+    '''
+    def __init__(self,dic_len,in_size,out_size):
+        super(multiLinear,self).__init__()
+        self.weight = Parameter(torch.randn(dic_len,out_size,in_size))
+        self.bias = Parameter(torch.randn(dic_len,out_size))
+    def forward(self,i,x):
+        return torch.matmul(self.weight[i,:,:],x)+self.bias[i,:]
+
+def __def_tensor__(gpunum,listobj):
+    '''
+    Helper Function.
+    This function defines a tensor considering whether or not to use the GPU.
+    '''
+    if gpunum < 0:
+        return autograd.Variable(torch.Tensor(listobj))
+    else:
+        with torch.cuda.device(gpunum):
+            return autograd.Variable(torch.cuda.Tensor(listobj))
+
 class SyntacticSemanticEngine(nn.Module):
     '''
     Syntactic Semantic Engine is a new model for representing a dependency
     tree along with the distributed representations of the corresponding words.
     '''
-    def __init__(self,dep_dict,pos_dict,GPUnum=0,sensedim=300,\
-            w2vdim=300,nonlin=F.relu):
+    def __init__(self,dep_dict,pos_dict,glove_voc,GPUnum=0,sensedim=300,\
+        nonlin=F.relu):
         '''
         To initiate, provide dictionaries that map to indices from dependency
         relations (dep_dict) and parts of speech (pos_dict).
@@ -27,30 +50,29 @@ class SyntacticSemanticEngine(nn.Module):
         # Sense vector size
         self.s = sensedim
         # Word vector size
-        self.N = w2vdim
+        self.N = len(glove_voc['the'])
         # Dependency vocabulary size
         self.d = len(dep_dict)
+        self.dep_dict = dep_dict
         # POS vocabulary size
         self.p = len(pos_dict)
+        self.pos_dict = pos_dict
+        # Save the word2vec dictionary
+        self.glove_voc = glove_voc
         # Use GPU or not and device number
         self.gpu = GPUnum
 
         # Define the network parameters
         # Initiate dependency embedding for each dependency relation type
-        self.D = {}
-        for a_dep in dep_dict:
-            if self.gpu<0:
-                self.D[a_dep]=F.Linear(self.s,self.s)
-            else:
-                self.D[a_dep]=F.Linear(self.s,self.s).cuda(self.gpu)
-
+        if self.gpu < 0:
+            self.D = multiLinear(self.d,self.s,self.s)
+        else:
+            self.D = multiLinear(self.d,self.s,self.s).cuda(self.gpu)
         # Initiate POS Embedding for each POS type
-        self.P = {}
-        for a_pos in pos_dict:
-            if self.gpu<0:
-                self.P[a_pos]=F.Linear(self.N,self.s)
-            else:
-                self.P[a_pos]=F.Linear(self.N,self.s).cuda(self.gpu)
+        if self.gpu < 0:
+            self.P = multiLinear(self.p,self.N,self.s)
+        else:
+            self.P = multiLinear(self.p,self.N,self.s).cuda(self.gpu)
         
         # Set nonlinearity
         self.nonlin=nonlin
@@ -60,19 +82,86 @@ class SyntacticSemanticEngine(nn.Module):
         Procedure to encode a single node in the tree
         '''
         # Input word vector
-        if self.gpu < 0:
-            xin = autograd.Variable(torch.Tensor(wvec))
-        else:
-            with torch.cuda.device(self.gpu):
-                xin = autograd.Variable(torch.cuda.Tensor(wvec))
-        u = self.nonlin(self.P[pos](xin)+hin)
-        hout = self.nonlin(self.D[dep](u))
+        xin = __def_tensor__(self.gpu,wvec)
+        u = self.nonlin(self.P(pos,xin)+hin)
+        hout = self.nonlin(self.D(dep,u))
         return hout
 
-    def encodetree(self,atree,glove_voc):
+    def encodetree(self,atree):
+        '''
+        Recursively encodes a dependency tree to its embedding vector
+        '''
+        hout_sum = None
         for i,anode in enumerate(atree):
-            if type(anode) == list:
-                pass
-            elif type(anode) == unicode or type(anode)==str:
-                pass
-                    
+            if type(anode) == unicode or type(anode)==str:
+                # lookahead if the next node is a subtree
+                if i < len(atree)-1 and type(atree[i+1])==list:
+                    # Next node is a subtree, process it first
+                    hin = self.encodetree(atree[i+1])
+                else:
+                    # This node doesn't have any child. set hin to zero
+                    hin = __def_tensor__(self.gpu,[0 for i in range(self.s)])
+                # Compute the current node
+                w,p,d = anode.strip().encode('ascii','ignore').split()
+                hout = self.__process_node__(self.glove_voc[w],\
+                    self.pos_dict[p],self.dep_dict[d],hin)
+                # Add all the children values together
+                if hout_sum is None:
+                    hout_sum = hout
+                else:
+                    hout_sum+=hout
+        return hout_sum
+
+    def forward(self,minibatch):
+        '''
+        Produce the model output of a minibatch
+        '''
+        treevec = None
+        for atree in minibatch:
+            if not atree:
+                raise IOError('Can not contain empty data')
+            if not treevec:
+                treevec = self.encodetree(atree)
+            else:
+                temp = self.encodetree(atree)
+                treevec=torch.cat([treevec,temp],dim=0)
+        return treevec
+
+def __test_encodetree__():
+    '''
+    For testing purpose only. Checks the encodetree function in the SSE
+    '''
+    wdict = {'thank':[0.,0.,1.],'you':[0,0,0.5],'so':[0.1,0.5,0.7],\
+    'much':[0,0.5,0.1],'chris':[0.1,0.2,0.1],',':[0.2,0.2,0.1],\
+    '.':[0.4,0.1,0.3],'the':[0.5,0.5,0.5]}
+    x = [u'thank VBP ROOT',[u'you PRP dobj',u'much RB advmod',\
+    [u'so RB advmod'],u', , punct',u'chris FW dobj',u'. . punct']]
+    _,dep_dict,_,pos_dict = ttdf.read_dep_pos_vocab()
+    model = SyntacticSemanticEngine(dep_dict,pos_dict,wdict,\
+        GPUnum=-1,sensedim=3)
+    y = model.encodetree(x)
+
+def test_with_multiLinear():
+    '''
+    For testing purpose only. This is a simple test to check if the
+    multiLinear module works or not.
+    Works perfectly.
+    '''
+    model = multiLinear(2,3,2)
+    optimizer = optim.Adam(model.parameters(),lr=0.001)
+    w1 = autograd.Variable(torch.Tensor([[1,0,0],[0,0,-1]]))
+    b1 = autograd.Variable(torch.Tensor([1,1]))
+    w2 = autograd.Variable(torch.Tensor([[0,-1,0],[1,0,0]]))
+    b2 = autograd.Variable(torch.Tensor([2,-2]))
+    for i in range(25000):
+        x1 = autograd.Variable(torch.rand(3))
+        y1 = torch.matmul(w1,x1)+b1
+        x2 = autograd.Variable(torch.rand(3))
+        y2 = torch.matmul(w2,x2)+b2
+        model.zero_grad()
+        loss = torch.sqrt(torch.pow(model(0,x1)-y1,2).sum()+\
+            torch.pow(model(1,x2)-y2,2).sum())
+        loss.backward()
+        optimizer.step()
+        print 'loss =',loss
+    print model.state_dict()                    
